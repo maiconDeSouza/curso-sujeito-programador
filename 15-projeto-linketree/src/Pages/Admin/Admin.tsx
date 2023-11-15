3ebdf1329cf8c46f9971a479790a55207492b6be
Maicon Souza <maicon_souza09@hotmail.com>
2023-11-15 20:27:54 -0300

    15-11-23 20:27

--- 15-projeto-linketree/src/Pages/Admin/Admin.tsx
+++ 15-projeto-linketree/src/Pages/Admin/Admin.tsx
@@ -1,17 +1,167 @@
+import { FormEvent, useEffect, useState } from "react";
 import { Header } from "../../components/Header/Header";
 import { Input } from "../../components/Input/Input";
+import { Trash } from "lucide-react";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+
+interface linksProps {
+    id: string
+    name: string
+    url: string
+    bg: string
+    color: string
+}
 
 export function Admin(){
+    const [ nameInput, setNameInput ] = useState("")
+    const [ urlInput, setUrlInput ] = useState("")
+    const [ textColorInput, setTextColorInput ] = useState("#f1f1f1")
+    const [ backgroundColorInput, setBackgroundColorInput ] = useState("#121212")
+    const [ links, setLinks ] = useState<linksProps[]>([])
+
+    useEffect(() => {
+        const linksRef = collection(db, "links")
+        const queryRef = query(linksRef, orderBy("created", "asc"))
+
+        const unsub = onSnapshot(queryRef, (snapshot) => {
+            const lista: linksProps[] = []
+
+            snapshot.forEach((doc) => {
+                lista.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    url: doc.data().url,
+                    bg: doc.data().bg,
+                    color: doc.data().color
+
+                })
+            })
+            setLinks(lista)
+        })
+
+        return () => {
+            unsub()
+        }
+    },[])
+
+    async function handleDeleteLink(id: string){
+        try {
+            const docRef = doc(db, "links", id)
+            await deleteDoc(docRef)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function handleRegister(e: FormEvent){
+        e.preventDefault()
+
+        if(!nameInput || !urlInput){
+            alert("Preencha todos os campos")
+            return
+        }
+
+        try {
+            await addDoc(collection(db, "links"),{
+                name: nameInput,
+                url: urlInput,
+                bg: backgroundColorInput,
+                color: textColorInput,
+                created: new Date()
+            })
+            setNameInput("")
+            setUrlInput("")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="flex items-center flex-col min-h-screen pb-7 px-2">
             <Header />
 
-            <form className="flex flex-col mt-8 mb-3 w-full max-w-xl">
+            <form className="flex flex-col mt-8 mb-3 w-full max-w-xl" onSubmit={handleRegister}>
                 <label className="text-white font-medium mt-2 mb-2">Nome do Link</label>
                 <Input 
                     placeholder="Digite o nome do link..."
+                    value={nameInput}
+                    onChange={(e) => setNameInput(e.target.value)}
                 />
+                <label className="text-white font-medium mt-2 mb-2">URL do Link</label>
+                <Input
+                    type="url" 
+                    placeholder="Digite a url do link..."
+                    value={urlInput}
+                    onChange={(e) => setUrlInput(e.target.value)}
+                />
+                <section className="flex my-4 gap-5">
+                    <div className="flex gap-2">
+                        <label htmlFor="textColorInput" className="text-white font-medium mt-2 mb-2">Cor do link</label>
+                        <input
+                            id="textColorInput"
+                            type="color"
+                            value={textColorInput}
+                            onChange={ e => setTextColorInput(e.target.value)} 
+                        />
+                    </div>
+
+                    <div className="flex gap-2">
+                        <label htmlFor="backgroundColorInput" className="text-white font-medium mt-2 mb-2">Fundo do link </label>
+                        <input
+                            id="backgroundColorInput"
+                            type="color"
+                            value={backgroundColorInput}
+                            onChange={ e => setBackgroundColorInput(e.target.value)} 
+                        />
+                    </div>
+                </section>
+                {
+                    nameInput && (
+                        <div className="flex items-center justify-start flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
+                            <label className="text-white font-medium mt-2 mb-2">Como está ficando o link</label>
+                            <article 
+                                className="w-11/12 max-w-lg flex flex-col items-center justify-between rounded px-1 py-3"
+                                style={{marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput, color: textColorInput}}
+                            >
+                                <p className="font-medium">{nameInput}</p>
+                            </article>
+                        </div>
+                    )
+                }
+                <button 
+                    type="submit" 
+                    className=" mb-7 bg-blue-600 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center"
+                >
+                    Cadastrar 
+                </button>
             </form>
+            <h2 className="font-bold text-white mb-4 text-2xl">
+                Meus Links
+            </h2>
+            {
+                links.map( link => {
+                    return (
+                        <article 
+                            key={link.id} 
+                            className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none"
+                            style={{backgroundColor: link.bg}}
+                        >
+                            <p style={{color: link.color}}>{link.name}</p>
+                        <div>
+                            <button 
+                                type="button" 
+                                title="trash" 
+                                className="border border-dashed p-2 rounded bg-neutral-900"
+                                onClick={() => handleDeleteLink(link.id)}
+                            >
+                                <Trash size={18} color="#fff" />
+                            </button>
+                        </div>
+            </article>
+                    )
+                })
+            }
         </div>
     )
 }
